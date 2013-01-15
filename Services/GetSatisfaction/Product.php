20ded9e65a4916b529d3defbf9b29c4e23e28c68
D Keith Casey Jr <keith@caseysoftware.com>
2013-01-15 00:35:34 -0600

    wired the first steps of the product retrieval

--- Services/GetSatisfaction/Product.php
+++ Services/GetSatisfaction/Product.php
@@ -2,8 +2,11 @@
 
 class Services_GetSatisfaction_Product extends Services_GetSatisfaction
 {
-    
-    protected $company = '';
+
+    protected $_company = '';
+
+    protected $_products_json = '';
+    protected $_products_obj  = null;
 
     /**
      * Constructor.
@@ -16,6 +19,58 @@ class Services_GetSatisfaction_Product extends Services_GetSatisfaction
     {
         parent::__construct($username, $password);
         
-        $this->company = $company;
+        $this->_company = $company;
+        $this->_loadProducts();
+    }
+
+    protected function _loadProducts()
+    {
+        $product_url = $this->_base_url . '/companies/' . $this->_company .
+                '/products.' . $this->_format;
+
+        $this->_products_json = $this->_get($product_url);
+        $this->_products_obj  = json_decode($this->_products_json);
+    }
+
+    public function getProductCount()
+    {
+        return $this->_products_obj->total;
     }
 }
+
+/*
+{
+	"total":2,
+	"data":[
+        {
+        	"description":"Discussion about OpenVBX Plugins",
+            "canonical_name":"openvbx_openvbx_plugins",
+            "created_at":"2010/06/25 19:42:28 +0000",
+            "url":"https://api.getsatisfaction.com/products/57937",
+            "links":[],
+            "company":"openvbx",
+            "name":"OpenVBX Plugins",
+            "image":"https://d37wxxhohlp07s.cloudfront.net/public/uploaded_images/4772167/plugin-icon_small.png",
+            "id":57937
+        },
+        {
+            "description":"OpenVBX is a web-based open source phone system for business, powered by Twilio.",
+            "canonical_name":"openvbx_openvbx",
+            "created_at":"2010/06/15 11:24:08 +0000",
+            "url":"https://api.getsatisfaction.com/products/57646",
+            "links":[
+                {
+                    "label":"OpenVBX.org",
+                    "created_at":"2010/06/16 00:23:10 +0000",
+                    "id":175734,
+                    "url":"http://www.openvbx.org"
+                }
+            ],
+            "company":"openvbx",
+            "name":"OpenVBX",
+            "image":"https://d37wxxhohlp07s.cloudfront.net/public/uploaded_images/4691793/openvbx-logo-temp_small.png",
+            "id":57646
+        }
+    ]
+}
+ * */
