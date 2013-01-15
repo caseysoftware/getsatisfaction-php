d382e58a00e66cbd0cba9fed7fa810575e867727
D Keith Casey Jr <keith@caseysoftware.com>
2013-01-15 02:25:21 -0600

    updated the list/aggregate object to be iterable;

--- Services/GetSatisfaction/Products.php
+++ Services/GetSatisfaction/Products.php
@@ -1,12 +1,13 @@
 <?php
 
-/*
- * @todo TODO: Should this be iterable so we can list them off individually?
- */
-class Services_GetSatisfaction_Products extends Services_GetSatisfaction_Resource
+class Services_GetSatisfaction_Products
+    extends Services_GetSatisfaction_Resource
+    implements Iterator
 {
     protected $_products_json = '';
-    protected $_products_obj  = null;
+    protected $_obj  = null;
+
+    protected $_index = 0;
 
     /**
      * Constructor.
@@ -29,22 +30,56 @@ class Services_GetSatisfaction_Products extends Services_GetSatisfaction_Resourc
                 '/products.' . $this->_format;
 
         $this->_products_json = $this->_get($product_url);
-        $this->_products_obj  = json_decode($this->_products_json);
+        $this->_obj  = json_decode($this->_products_json);
     }
 
     public function getProductCount()
     {
-        return $this->_products_obj->total;
+        return $this->_obj->total;
     }
 
     public function getProduct($index = 0)
     {
         $product = array();
 
-        if (isset($this->_products_obj->data[$index])) {
-            $product = $this->_products_obj->data[$index];
+        if (isset($this->_obj->data[$index])) {
+            $product = $this->_obj->data[$index];
         }
 
         return new Services_GetSatisfaction_Product($product);
+    }
+
+    /**
+     * This is a duplicate of getProduct so that we can have a commonly named
+     *    method across all of our resources. It makes the iterator work more
+     *    easily.
+     *
+     * @param type $index
+     * @return type
+     */
+    public function getObject($index = 0)
+    {
+        return $this->getProduct($index);
+    }
+
+    public function rewind()
+    {
+        $this->_index = 0;
+    }
+    public function current()
+    {
+        return $this->getProduct($this->_index);
+    }
+    public function key()
+    {
+        return $this->_index;
+    }
+    public function next()
+    {
+        $this->_index++;
+    }
+    public function valid()
+    {
+        return isset($this->_obj->data[$this->_index]);
     }
 }
